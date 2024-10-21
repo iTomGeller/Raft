fb9b6dd1cad8876e5af3603fff83631d5f6ec152
iTomGeller <303327065@qq.com>
2024-10-21 21:01:03 +0800

    fix the __size_of_block bug
     caused by incorrect type-casting

--- buffer.h
+++ buffer.h
@@ -4,35 +4,36 @@
 #include <memory>
 #include <cstring>
 
-namespace raft{
+namespace raft
+{
 
-class buffer;
-using bufptr =  std::shared_ptr<buffer>;//定义成unique_ptr不好传递
+    class buffer;
+    using bufptr = std::unique_ptr<buffer>; // 定义成unique_ptr不好传递
 
-typedef unsigned char byte;
+    typedef unsigned char byte;
 #define sz_int 4
 #define sz_ulong 8
-//pos拿来判断 d拿来取数据
-class buffer{
-public:
-    //static说明是属于这个类而不是任何一个实例
-    static bufptr alloc(ssize_t size);
-    ssize_t pos() const;
-    byte* data() const;//返回data所在的起始位置 
-    //因为是用new申请以字节为单位的内存 所以是byte
-    ssize_t size() const;
-    void pos(ssize_t p);
+    // pos拿来判断 d拿来取数据
+    class buffer
+    {
+    public:
+        // static说明是属于这个类而不是任何一个实例
+        static bufptr alloc(ssize_t size);
+        ssize_t pos() const;
+        byte *data() const; // 返回data所在的起始位置
+        // 因为是用new申请以字节为单位的内存 所以是byte
+        ssize_t size() const;
+        void pos(ssize_t p);
 
-    void put(int val);
-    void put(ulong  val);
-    void put(std::string str);
-    void put(const buffer& buf);
+        void put(int val);
+        void put(ulong val);
+        void put(std::string str);
+        void put(const buffer &buf);
 
-    int get_int();
-    std::string get_str(ssize_t len);
-    ulong get_ulong();
-    byte get_byte();
-
-};
-}//namespace raft
+        int get_int();
+        std::string get_str(ssize_t len);
+        ulong get_ulong();
+        byte get_byte();
+    };
+} // namespace raft
 #endif
