c98070012763cc09c2baaf9409340d05a31bc050
iTomGeller <303327065@qq.com>
2024-11-04 20:45:35 +0800

    Update rpc_cli.h

--- rpc_cli.h
+++ rpc_cli.h
@@ -1,12 +1,23 @@
-#ifndef RPC_CLI_H
-#define RPC_CLI_H
-#include "buffer.h"
+#ifndef _RPC_CLI_H_
+#define _RPC_CLI_H_
+#include <exception>
+#include "async.h"
+#include "req_msg.h"
+#include "resp_msg.h"
+#include "rpc_exception.h"
+
 namespace raft
 {
-    class rpc_client
-    {
-    public:
-        virtual void send(bufptr &buf) = 0;
-    };
+using rpc_result = async_result<ptr<resp_msg>, ptr<rpc_exception>>;
+using rpc_handler = rpc_result::handler_type;
+
+class rpc_client
+{
+    __interface_body__(rpc_client);
+
+public:
+    virtual void send(ptr<req_msg>& req, rpc_handler& when_done) = 0;
+};
 } // namespace raft
-#endif
+
+#endif //_RPC_CLI_H_
