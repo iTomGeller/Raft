26d462bae99643d2f02a30819ad2d085ce4427ad
iTomGeller <303327065@qq.com>
2024-11-04 20:45:48 +0800

    Create snapshot.h

---
+++ snapshot.h
@@ -0,0 +1,52 @@
+#ifndef _SNAPSHOT_HXX_
+#define _SNAPSHOT_HXX_
+
+#include "buffer.h"
+#include "cluster_config.h"
+
+namespace raft
+{
+class snapshot
+{
+public:
+    snapshot(ulong last_log_idx, ulong last_log_term, const ptr<cluster_config>& last_config, ulong size = 0)
+        : last_log_idx_(last_log_idx), last_log_term_(last_log_term), size_(size), last_config_(last_config)
+    {
+    }
+
+    __nocopy__(snapshot);
+
+public:
+    ulong get_last_log_idx() const
+    {
+        return last_log_idx_;
+    }
+
+    ulong get_last_log_term() const
+    {
+        return last_log_term_;
+    }
+
+    ulong size() const
+    {
+        return size_;
+    }
+
+    const ptr<cluster_config>& get_last_config() const
+    {
+        return last_config_;
+    }
+
+    static ptr<snapshot> deserialize(buffer& buf);
+
+    bufptr serialize();
+
+private:
+    ulong last_log_idx_;
+    ulong last_log_term_;
+    ulong size_;
+    ptr<cluster_config> last_config_;
+};
+} // namespace raft
+
+#endif
