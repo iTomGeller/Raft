06e1eef7b8e204ec4c143046d0e29a209643211e
iTomGeller <303327065@qq.com>
2024-12-02 14:14:06 +0800

    Update buffer.h

--- include/buffer.h
+++ include/buffer.h
@@ -12,33 +12,38 @@ namespace raft
 {
 
     class buffer;
-    using bufptr = std::unique_ptr<buffer>; // 定义成unique_ptr不好传递
-
+    //using bufptr = std::unique_ptr<buffer>; // 定义成unique_ptr不好传递
+    using bufptr = uptr<buffer, void (*)(buffer*)>;
     // pos拿来判断 d拿来取数据
     class buffer
     {
+       buffer() = delete;
+    __nocopy__(buffer);
     public:
         // static说明是属于这个类而不是任何一个实例
-        static bufptr alloc(ssize_t size);
+        static bufptr alloc(size_t size);
         static bufptr copy(const buffer& buf);
         
-        ssize_t pos() const;
-        byte *data() const; // 返回data所在的起始位置
-        // 因为是用new申请以字节为单位的内存 所以是byte
-        ssize_t size() const;
-        void pos(ssize_t p);
+        size_t size() const;
+        size_t pos() const;
+        void pos(size_t p);
         
-        void put(byte b);
-        void put(int val);
-        void put(ulong val);
-        void put(std::string str);
-        void put(const buffer &buf);
-
-        int get_int();
-        const char * get_str();
+        
+        int32 get_int();
         ulong get_ulong();
         byte get_byte();
-        void get_buf(bufptr& dst);
+        //void get_buf(bufptr& dst);
+        void get(bufptr& dst);
+        const char * get_str();
+        byte *data() const; // 返回data所在的起始位置
+        // 因为是用new申请以字节为单位的内存 所以是byte
+
+        void put(byte b);
+        void put(int32 val);
+        void put(ulong val);
+        //void put(std::string str);
+        void put(const std::string& str);
+        void put(const buffer &buf);
     };
     std::ostream& operator<<(std::ostream& out, buffer& buf);
     std::istream& operator>>(std::istream& in, buffer& buf);
